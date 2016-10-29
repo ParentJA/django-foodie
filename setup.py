00a54181dceee003c8ea1beca52405e32c01a138
Jason Parent <ParentJ@advisory.com>
2016-10-29 10:53:31 -0400

    Updated README.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 __author__ = 'Jason Parent'
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     readme = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
